5b1758cebc6c0e5196c0e8f7d4b4ee42e934263a
Pongsapuk Swaroj <78994037+codeBrewer216@users.noreply.github.com>
2026-06-23 10:23:43 +0700

    feat: Update home page to dynamically display movie list and enhance styling for headings

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,28 @@
-import { Component, } from '@angular/core';
-
+import { Component, inject, signal, } from '@angular/core';
+import { AuthService } from '../../services/auth';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { Movie, MoviesService } from '../../services/movies';
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule, RouterLink,],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
 export class Home {
+  private moviesService = inject(MoviesService);
+  private authService = inject(AuthService);
 
+
+  moviesList = signal<Movie[]>([]);
+  isLogined = this.authService.isLoggedIn()
+
+  ngOnInit() {
+    this.moviesService.getList().subscribe({
+      next: (res: Movie[]) => {
+        this.moviesList.set(res)
+      }
+    })
+  }
 }
